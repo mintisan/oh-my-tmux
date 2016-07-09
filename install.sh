73f805d74a0c9ecc23cde8350ce9b58ddbf9a1d8
mintisan <mint@mail.ustc.edu.cn>
2016-07-09 16:25:17 +0000

    rename .tmux.conf to tmux.conf

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
-
-
 git clone https://github.com/mintisan/oh-my-tmux.git ~/oh-my-tmux
 cd ~/oh-my-tmux
-ln -s $PWD/oh-my-tmux/.tmux.conf ~/.tmux.conf
+ln -s $PWD/tmux.conf ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 tmux source ~/.tmux.conf
