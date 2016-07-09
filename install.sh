a8b4257338e18c2f57a40455882667c1967dc57f
Ted Lin <fovwin@gmail.com>
2016-07-09 08:29:18 +0000

    Update install.sh
    add remove old tmux config

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+echo "remove old tmux config, u should backuo firstly"
+sudo rm -rf ~/.tmux ~/oh-my-tmux ~/.tmux.conf
+
 git clone https://github.com/mintisan/oh-my-tmux.git ~/oh-my-tmux
 cd ~/oh-my-tmux
 ln -s $PWD/tmux.conf ~/.tmux.conf
